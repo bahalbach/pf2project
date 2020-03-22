7011e8a0998784829205fc207bfaf2587647735e
bahalbach <bahalbach@gmail.com>
2020-03-22 21:58:26 +0800

    added remove 1 die

--- pf2calcAttacks.py
+++ pf2calcAttacks.py
@@ -1406,6 +1406,11 @@ class AtkSelection:
                             self.runeDamageDice[i]+=[d4]
                     if feature[1] < 21:
                         self.details += "[" + str(feature) + "]\n"
+                elif feature[0] == "Remove 1 Die":
+                    for i in self.weaponDamageDice:
+                        if i >= feature[1]:
+                            if len(self.weaponDamageDice[i]) > 0:
+                                self.weaponDamageDice[i].pop()
                 elif feature[0] == "backswing":
                     self.setBackswing(feature[1])
                     if feature[1] < 21:
